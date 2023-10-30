aefb1b1d9f8f8993670aa1628f1e86edb7f6c63f
Lycia Stenson <66551408+LennonMcCartney@users.noreply.github.com>
2023-10-30 22:07:37 +0000

    Created room scene

--- Scripts/Bottle.gd
+++ Scripts/Bottle.gd
@@ -1,5 +1,5 @@
 @tool
-extends MeshInstance3D
+extends Node3D
 
 @export var width : float
 @export var height : float
@@ -11,11 +11,14 @@ extends MeshInstance3D
 @export_color_no_alpha var liquid_colour : Color
 @export_color_no_alpha var surface_colour : Color
 
-@onready var liquid_shader : ShaderMaterial = mesh.surface_get_material(0).next_pass as ShaderMaterial
+@onready var rigid_body : RigidBody3D = $RigidBody
+@onready var mesh_instance : MeshInstance3D = $RigidBody/CollisionShape/MeshInstance
+
+@onready var liquid_shader : ShaderMaterial = mesh_instance.mesh.surface_get_material(0).next_pass as ShaderMaterial
 @onready var surface_shader : ShaderMaterial = liquid_shader.next_pass as ShaderMaterial
 
-func _ready():
-	pass
+var dragging : bool = false
+var mouse_relative : Vector2
 
 func _process(_delta):
 	liquid_shader.set_shader_parameter("fill_amount", fill_amount)
@@ -34,4 +37,30 @@ func _process(_delta):
 	surface_shader.set_shader_parameter("surface_colour", surface_colour)
 
 func _physics_process(_delta):
-	pass
+	if (dragging and rigid_body.linear_velocity.length_squared() < 30.0):
+		var mouse_y : float = -mouse_relative.y / 2.0
+		var mouse_x : float = mouse_relative.x / 6.0
+		rigid_body.apply_central_force(Vector3(mouse_x, mouse_y, 0.0))
+		mouse_relative = Vector2(0.0,0.0)
+
+func _input(event):
+	if (event.is_action_pressed("left_mouse_button")):
+		var space = get_world_3d().direct_space_state
+		var camera : Camera3D = get_viewport().get_camera_3d()
+		var mouse_pos : Vector2 = get_viewport().get_mouse_position()
+		var ray_from : Vector3 = camera.project_ray_origin(mouse_pos)
+		var ray_to : Vector3 = ray_from + camera.project_ray_normal(mouse_pos) * 100000
+		var ray_query := PhysicsRayQueryParameters3D.create(ray_from, ray_to)
+		var result := space.intersect_ray(ray_query)
+		if (result and result.collider == rigid_body):
+			rigid_body.gravity_scale = 0
+			rigid_body.linear_damp = 10
+			rigid_body.angular_damp = 10
+			dragging = true
+	elif (event.is_action_released("left_mouse_button")):
+		rigid_body.gravity_scale = 1
+		rigid_body.linear_damp = 1
+		rigid_body.angular_damp = 1
+		dragging = false
+	if (dragging and event is InputEventMouseMotion):
+		mouse_relative = event.relative
